b46aa3a369e19e78e7539caff4ab65e91515a48b
yindaheng98 <yindaheng98@163.com>
2020-07-24 20:42:46 +0800

    删除了无用文件

--- deploy.sh
+++
@@ -1,9 +0,0 @@
-git config user.name "TravisCI"
-git config user.email "yindaheng98@163.com"
-cd public
-set -e
-git init
-git add -A
-git commit -m 'TravisCI Deploy'
-set -e
-git push -u https://$1@github.com/yindaheng98/yindaheng98.github.io.git HEAD:master --force
